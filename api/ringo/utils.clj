cd889071647cc0c4627dc98726729643dc4cf384
priyatam <priyatam@gmail.com>
2014-09-29 03:36:03 -0700

    clean

--- api/ringo/utils.clj
+++ api/ringo/utils.clj
@@ -12,12 +12,3 @@
 
 (defmacro mock [val body]
   val)
-
-(defmacro if-lets
-  "Bind multiple bindings"
-  ([bindings then-exp]
-     (let [values (take-nth 2 (rest bindings))]
-       `(if (and ~@values) (let ~bindings ~then-exp) false)))
-  ([bindings then-exp else-exp]
-     (let [values (take-nth 2 (rest bindings))]
-       `(if (and ~@values) (let ~bindings ~then-exp) ~else-exp))))
