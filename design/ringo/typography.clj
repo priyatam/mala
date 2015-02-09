732f00b7f1b34aae0c190fb63d05594d6f1b8c7b
priyatam <priyatam@gmail.com>
2015-02-09 15:37:18 -0800

    fix order of declarations

--- design/ringo/typography.clj
+++ design/ringo/typography.clj
@@ -17,18 +17,6 @@
             :ff-sans ["Fira Sans" "sans-serif"]
             :ff-mono ["Source Code Pro" "monospace"]})
 
-;; Basic
-
-(def typesetting
-  [[:body :p (font font-family-sans-serif 2 300 0.1 1.5)]
-   [:h1 (font font-family-sans-serif 3 600 0.5 2)]
-   [:h2 (font font-family-sans-serif 3 400 0.5 2)]
-   [:h3 (font font-family-serif 2 300 0.5 2)]
-   [:h4 (font font-family-serif 1.5 300 0.3 2)]
-   [:h5 :h6 (font font-family-serif 1.5 300 0.3 1.5)]
-   [:header (font font-family-sans-serif 4 700 0.3 1.5 "small-caps")]
-   [:footer (font font-family-sans-serif 1 100 0.3 1.5)]])
-
 ;; Mixins
 
 (defn font [family size weight letter-spacing line-height & options]
@@ -56,6 +44,16 @@
             [:container {:width (px width)}]))
 
 ;; Elements
+
+(def typesetting
+  [[:body :p (font font-family-sans-serif 2 300 0.1 1.5)]
+   [:h1 (font font-family-sans-serif 3 600 0.5 2)]
+   [:h2 (font font-family-sans-serif 3 400 0.5 2)]
+   [:h3 (font font-family-serif 2 300 0.5 2)]
+   [:h4 (font font-family-serif 1.5 300 0.3 2)]
+   [:h5 :h6 (font font-family-serif 1.5 300 0.3 1.5)]
+   [:header (font font-family-sans-serif 4 700 0.3 1.5 "small-caps")]
+   [:footer (font font-family-sans-serif 1 100 0.3 1.5)]])
 
 (def defaults
   [:body :p :h1 :h2 (font font-family-sans-serif 2 300 0.3 2)])
