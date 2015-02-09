e24b8df0fd5fb56cbb3045729470152de4fb00d4
priyatam <priyatam@gmail.com>
2015-02-09 15:02:06 -0800

    deleted generated js

--- resources/public/js/components.js
+++
@@ -1,1 +0,0 @@
-if(typeof goog == "undefined") document.write('<script src="js/out/goog/base.js"></script>');document.write('<script src="js/out/cljs_deps.js"></script>');document.write('<script>if (typeof goog != "undefined") { goog.require("ringo.components"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
