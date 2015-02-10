8fab9c9086e45ff3fef03f1ae004c968a79d696c
priyatam <priyatam@gmail.com>
2015-02-09 23:55:39 -0800

    replaced cljs-ajax with cljs-http (core.async)

--- ui/ringo/client.cljs
+++ ui/ringo/client.cljs
@@ -1,10 +1,8 @@
 (ns ringo.client
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [ajax.core :refer [GET POST]]
+  (:require [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
             [ringo.utils :refer [log guid debug]]))
-
-(def host "http://localhost:3449/" )
 
 (defn handler [response]
   (.log js/console (str response)))
@@ -12,6 +10,6 @@
 (defn error-handler [{:keys [status status-text]}]
   (.log js/console (str "Error: " status " " status-text)))
 
-(defn getData [dname]
-  (GET (str "/" dname {:handler handler
-                       :error-handler error-handler})))
+(defn GET [res handler]
+  (go (let [response (<! (http/get res {:with-credentials? false}))]
+        (handler (:body response)))))
--- ui/ringo/components.cljs
+++ ui/ringo/components.cljs
@@ -2,9 +2,8 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [om.core :as om  :include-macros true]
             [cljs.core.async :refer [<! chan put! sliding-buffer]]
-            [ajax.core :refer (GET)]
             [sablono.core :as html :refer-macros [html]]
-            [ringo.client :as client]))
+            [ringo.client :as client :refer [GET]]))
 
 (enable-console-print!)
 
@@ -20,21 +19,24 @@
   (let [host (:url (om/get-shared owner))
         {:keys [id type]} message
         url (str host "api/device/" id "/type/" type "/measurements")]
-    (GET url {:handler #(om/update! cursor [:data] %)})))
+    (GET url #(om/update! cursor [:data] %))))
 
 (defn- draw-chart [cursor {:keys [div bounds x-axis y-axis plot]}]
   (let [{:keys [id width height]} div
         Chart        (.-chart js/dimple)
         svg          (.newSvg js/dimple (str "#" id) width height)
         data         (get-in cursor [:data])
-        dimple-chart (.setBounds (Chart. svg) (:x bounds) (:y bounds) (:width bounds) (:height bounds))
+        dimple-chart (.setBounds (Chart. svg) (:x bounds) (:y bounds)
+                                 (:width bounds)
+                                 (:height bounds))
         x            (.addCategoryAxis dimple-chart "x" x-axis)
         y            (.addMeasureAxis dimple-chart "y" y-axis)
         s            (.addSeries dimple-chart nil plot (clj->js [x y]))]
     (aset s "data" (clj->js data))
     (.addLegend dimple-chart "5%" "10%" "20%" "10%" "right")
     (.draw dimple-chart)
-    (.attr (.selectAll (.-shapes x) "text") "transform" "rotate(45,0,12.6015625) translate(5, 0)")))
+    (.attr (.selectAll (.-shapes x) "text")
+           "transform" "rotate(45,0,12.6015625) translate(5, 0)")))
 
 ;; COMPONENTS ;;
 
@@ -60,24 +62,24 @@
       (when (seq (:data cursor))
         (draw-chart cursor chart)))))
 
-; Form containing list of devices that can be plotted
+;; Form containing list of devices that can be plotted
 
 (defn form-row [event-chan]
   (fn [the-item owner]
     (om/component
-     (let [{:keys [id type description unit]} the-item]
-       (html
-        [:tr
-         [:td [:input {:type "radio"
-                       :name "type"
-                       :value name
-                       :on-change (fn [e]
-                                    (put! event-chan {:id id
-                                                      :type type}))}]]
-         [:td id]
-         [:td type]
-         [:td description]
-         [:td unit]])))))
+      (let [{:keys [id type description unit]} the-item]
+        (html
+         [:tr
+          [:td [:input {:type "radio"
+                        :name "type"
+                        :value name
+                        :on-change (fn [e]
+                                     (put! event-chan {:id id
+                                                       :type type}))}]]
+          [:td id]
+          [:td type]
+          [:td description]
+          [:td unit]])))))
 
 (defn device-form
   [cursor owner]
@@ -86,13 +88,14 @@
     (will-mount [_]
       (let [host (:url (om/get-shared owner))
             url (str host "api/devices")]
-        (GET url {:handler #(om/update! cursor [:all] %)})))
+        (GET url #(om/update! cursor [:all] %))))
     om/IRenderState
     (render-state [_ {:keys [event-chan]}]
       (let [devices (:all cursor)]
         (html
          [:div
-          [:table {:class "table table-striped table-bordered table-condensed" :style {:width "100%"}}
+          [:table {:class "table table-striped table-bordered table-condensed"
+                   :style {:width "100%"}}
            [:thead [:tr
                     [:th "Select"]
                     [:th "ID"]
@@ -132,13 +135,13 @@
 (defn widget [data]
   (om/component
     (html
-       [:div [:h2 "Hello Ringo!"]])))
+     [:div [:h2 "Hello Ringo!"]])))
 
 ;; ROOT BINDING ;;
 
 (om/root widget {}
-  {:target (.getElementById js/document "hello")})
+         {:target (.getElementById js/document "hello")})
 
 (om/root chart-http app-model
-  {:target (.getElementById js/document "app")
-   :shared {:url client/host}})
+         {:target (.getElementById js/document "app")
+          :shared {:url client/host}})
