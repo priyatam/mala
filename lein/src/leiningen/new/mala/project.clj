b0bbbab830b52d3039e9967b983a0d4f02a5faa3
Priyatam Mudivarti <priyatam@gmail.com>
2015-05-18 17:46:42 -0700

    up lein template with deps

--- lein/src/leiningen/new/mala/project.clj
+++ lein/src/leiningen/new/mala/project.clj
@@ -1,4 +1,4 @@
-(defproject {{name}} "0.2.0-SNAPSHOT"
+(defproject {{name}} "0.1.0"
   :description "A starterkit for building apps with Garden, Ring, and Om"
   :url "https://github.com/author/{{name}}"
   :license {:name "Eclipse Public License"
@@ -9,23 +9,23 @@
   :jvm-opts ["-Xms768m" "-Xmx768m"]
   :global-vars {*warn-on-reflection* false *assert* false}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [ring/ring-core "1.3.2"]
-                 [ring/ring-defaults "0.1.3"]
-                 [ring/ring-headers "0.1.1"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-headers "0.1.3"]
                  [ring/ring-json "0.3.1"]
                  [fogus/ring-edn "0.2.0"]
-                 [ring-cors "0.1.6"]
-                 [compojure "1.3.1"]
-                 [cljs-http "0.1.25"]
+                 [ring-cors "0.1.7"]
+                 [compojure "1.3.4"]
+                 [cljs-http "0.1.30"]
                  [http-kit "2.1.18"]
                  [org.omcljs/om "0.8.8"]
                  [sablono "0.3.4"]
-                 [secretary "1.2.1"]
+                 [secretary "1.2.3"]
                  [garden "1.2.5"]
-                 [prone "0.8.0"]
+                 [prone "0.8.1"]
                  [environ "1.0.0"]]
 
   :source-paths ["src" "tasks" "target/classes"]
@@ -42,10 +42,11 @@
                                 :cache-analysis true
                                 :source-map true}}}}
 
-  :profiles {:dev {:dependencies [[figwheel "0.2.5-SNAPSHOT"]
-                                  [figwheel-sidecar "0.2.5-SNAPSHOT"]
-                                  [ring-mock "0.1.5"]
-                                  [ring/ring-devel "1.3.1"]]
+  :profiles {:dev {:dependencies [[figwheel "0.3.3"]
+                                  [figwheel-sidecar "0.3.3"]
+                                  [ring/ring-mock "0.2.0"]
+                                  [ring/ring-devel "1.3.1"]
+                                  [precursor/om-i "0.1.6"]]
                    :env {:is-dev true}
                    :cljsbuild {:builds
                                {:app {:source-paths ["env/dev"]}}}
@@ -78,10 +79,10 @@
                                                       :optimizations :advanced
                                                       :pretty-print false}}}}}}
 
-  :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-figwheel "0.2.5-SNAPSHOT"]
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [lein-figwheel "0.3.3"]
             [lein-environ "1.0.0"]
-            [lein-ring "0.9.1"]
+            [lein-ring "0.9.3"]
             [lein-kibit "0.0.8"]
             [lein-pprint "1.1.1"]
             [lein-garden "0.2.5"]
