39677181850a34c05b7559568e19175fda13030b
Priyatam Mudivarti <priyatam@gmail.com>
2015-05-26 17:40:16 -0700

    added state for "page"

--- src/ui/state.cljs
+++ src/ui/state.cljs
@@ -19,4 +19,35 @@
                            (sort (into [] (take 100 (repeatedly rand-word))))
                            (range)))
          :menu [{:name "Typeahead" :path "/typeahead"}
-                {:name "Dimple" :path "/graph"}]}))
+                {:name "Dimple" :path "/graph"}
+                {:name "Mesh" :path "/mesh"}]
+         :content {:title "Thinking Clojurescript"
+                   :issue "Issue #1"
+                   :publisher "The Facjure Review"
+                   :logo "http://placekitten.com/404/404"
+                   :editorial "Object Oriented programming as a paradigm has many real
+                   benefits but one of the worst plagues it has inflicted on
+                   programming culture is obscuring data. Functional programming
+                   is not a silver bullet but its emphasis on unadorned data is
+                   a guiding light. No models."
+                   :interviews ["Bruce Hauman, " "David Nolen, " "Joel Holdbrooks"]
+                   :developer-pic "http://placekitten.com/404/404"
+                   :essays ["“A meditation in Edn” by Priyatam Mudivarti"]
+                   :quotes {:nolen-tweet "“We create things with our tools, and
+                    our tools have consequences”"
+                            :dev-quote "The Functional final frontier: Clojurescript"}}}))
+
+(def history
+  (atom [@app-state]))
+
+(add-watch
+ app-state :history
+ (fn [_ _ old new]
+   #_(println old new history)
+   (when-not (= (last @history) new)
+     (swap! history conj new))))
+
+(defn undo! []
+  (when (> (count @history) 1)
+    (swap! history pop)
+    (reset! app-state (last @history))))
