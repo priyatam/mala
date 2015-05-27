9882796af8d2e1c54da85b2a1c4cfa4d9712b01e
Priyatam Mudivarti <priyatam@gmail.com>
2015-05-26 17:38:47 -0700

    delete old styles

--- src/design/styles.clj
+++
@@ -1,9 +0,0 @@
-(ns design.styles
-  (:require [garden.def :refer [defstyles defrule defkeyframes]]
-            [garden.core :refer [css]]
-            [design.components :as components]
-            [design.typography :as typography]
-            [design.layout :as layout]))
-
-(def all
-  (merge components/styles layout/styles typography/styles))
