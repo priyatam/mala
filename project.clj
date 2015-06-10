3dce63cb732163cf2371a90b2dc46739c600ee59
Priyatam Mudivarti <priyatam@gmail.com>
2015-06-10 14:25:03 +0900

    up cljs/clj versions

--- project.clj
+++ project.clj
@@ -9,8 +9,8 @@
   :jvm-opts ["-Xms768m" "-server"]
   :global-vars {*warn-on-reflection* false *assert* false}
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
-                 [org.clojure/clojurescript "0.0-3211"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/clojurescript "0.0-3308"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.omcljs/om "0.8.8"]
                  [sablono "0.3.4"]
@@ -19,7 +19,7 @@
                  [garden "1.2.5"]
                  [facjure/mesh "0.2.7"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]
+  :plugins [[lein-cljsbuild "1.0.6"]
             [com.cemerick/clojurescript.test "0.2.3"]
             [lein-figwheel "0.3.3"]
             [lein-garden "0.2.5"]
