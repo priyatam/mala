adf2d673265cc157051c1e602f566fa467c0d570
Priyatam Mudivarti <priyatam@gmail.com>
2016-01-18 01:29:49 -0800

    bump deps

--- project.clj
+++ project.clj
@@ -1,25 +1,28 @@
-(defproject mala "0.5.1"
-  :description "A integrated project template for building and designing UIs in Clojurescript"
+(defproject mala "0.5.2"
+  :description "A integrated UI template for building and designing pages in Clojurescript"
   :url "https://github.com/priyatam/mala"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/priyatam/mala"}
   :min-lein-version "2.5.0"
-  :jvm-opts ["-Xms768m" "-server"]
+  :jvm-opts ["-Xmx256m" "-server"]
   :global-vars {*warn-on-reflection* false *assert* false}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.28"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/core.async "0.2.374"]
                  [org.omcljs/om "0.9.0"]
-                 [sablono "0.3.6"]
+                 [cljsjs/react "0.14.3-0"]
+                 [cljsjs/react-dom "0.14.3-1"]
+                 [cljsjs/react-dom-server "0.14.3-0"]
+                 [facjure/mesh "0.4.0"]
+                 [sablono "0.5.3"]
                  [secretary "1.2.3"]
-                 [cljs-http "0.1.37"]
-                 [facjure/mesh "0.4.0"]]
+                 [cljs-http "0.1.39"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-figwheel "0.3.3"]
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-figwheel "0.5.0"]
             [lein-garden "0.2.6"]
             [lein-pdo "0.1.1"]]
 
@@ -56,13 +59,14 @@
                                 :pretty-print? false}}]}
 
   :profiles {:dev {:env {:is-dev true}
-                   :dependencies [[figwheel "0.3.3"]
-                                  [figwheel-sidecar "0.3.3"]
-                                  [ring/ring-json "0.3.1"]
+                   :dependencies [[figwheel "0.5.0"]
+                                  [figwheel-sidecar "0.5.0"]
+                                  [com.cemerick/piggieback "0.2.1"]
+                                  [ring/ring-json "0.4.0"]
                                   [fogus/ring-edn "0.2.0"]
-                                  [compojure "1.3.4"]
                                   [javax.servlet/servlet-api "2.5"]
                                   [precursor/om-i "0.1.7"]
+                                  [compojure "1.4.0"]
                                   [ankha "0.1.4"]]
                    :figwheel {:http-server-root "public"
                               :server-port 3449
